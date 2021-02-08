ed274c415b56424b2909c8e5b4e56ddd285b5f28
EMSI-zero <emad.ferdowsi@outlook.com>
2021-02-08 21:06:03 +0330

    -adds Node class -adds InputString class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,19 @@ package com.company;
 
 public class Main {
 
+    public class InputString{
 
+    }
+
+    public class Node{
+        char data;
+        Node next;
+        Node prev;
+
+        public Node(char data) {
+            this.data = data;
+        }
+    }
 
     public static void main(String[] args) {
 
